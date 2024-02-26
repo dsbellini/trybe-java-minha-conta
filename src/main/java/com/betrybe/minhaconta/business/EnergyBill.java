4082b2da9fbfe3956465d188f8cacc5dcdf6f4f5
Danilo Bellini <bellini.daan@gmail.com>
2024-02-26 17:49:16 -0300

    first commit

--- src/main/java/com/betrybe/minhaconta/business/EnergyBill.java
+++ src/main/java/com/betrybe/minhaconta/business/EnergyBill.java
@@ -1,5 +1,6 @@
 package com.betrybe.minhaconta.business;
 
+import com.ions.lightdealer.sdk.model.Address;
 import com.ions.lightdealer.sdk.model.ElectronicDevice;
 
 /**
@@ -7,6 +8,15 @@ import com.ions.lightdealer.sdk.model.ElectronicDevice;
  */
 public class EnergyBill {
   // Req. 1 – Create class constructor and attributes.
+
+  Address address;
+  boolean residentialPlan;
+  double rate = 0.15;
+
+  public EnergyBill(Address address, boolean residentialPlan) {
+    this.address = address;
+    this.residentialPlan = residentialPlan;
+  }
 
   /**
    * Req. 2 – Calculates an adjusted tariff for non-residential plans.

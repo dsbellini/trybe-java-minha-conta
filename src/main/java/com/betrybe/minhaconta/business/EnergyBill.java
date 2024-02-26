37ae963025cd48dae07608991c6ed4bae935c9b1
Danilo Bellini <bellini.daan@gmail.com>
2024-02-26 18:43:48 -0300

    feat: calculateTotalUsage implemented + Java Docs in EnergyBill

--- src/main/java/com/betrybe/minhaconta/business/EnergyBill.java
+++ src/main/java/com/betrybe/minhaconta/business/EnergyBill.java
@@ -9,10 +9,25 @@ import com.ions.lightdealer.sdk.model.ElectronicDevice;
 public class EnergyBill {
   // Req. 1 – Create class constructor and attributes.
 
+  /**
+   * The Address.
+   */
   Address address;
+  /**
+   * The Residential plan.
+   */
   boolean residentialPlan;
+  /**
+   * The Rate.
+   */
   double rate = 0.15;
 
+  /**
+   * Instantiates a new Energy bill.
+   *
+   * @param address         the address
+   * @param residentialPlan the residential plan
+   */
   public EnergyBill(Address address, boolean residentialPlan) {
     this.address = address;
     this.residentialPlan = residentialPlan;
@@ -20,6 +35,9 @@ public class EnergyBill {
 
   /**
    * Req. 2 – Calculates an adjusted tariff for non-residential plans.
+   *
+   * @param value the value
+   * @return the double
    */
   public double adjustedTariff(double value) {
     if (!residentialPlan) {
@@ -30,13 +48,22 @@ public class EnergyBill {
 
   /**
    * Req. 3 – Calculates the total usage of a collection of devices.
+   *
+   * @param devices the devices
+   * @return the int
    */
   public static int calculateTotalUsage(ElectronicDevice[] devices) {
-    return 0;
+    int total = 0;
+    for (ElectronicDevice device : devices) {
+      total += (int) device.monthlyKwh();
+    }
+    return total;
   }
 
   /**
-   * Aux. Method that estimates the energy bill value.
+   * deop Aux. Method that estimates the energy bill value.
+   *
+   * @return the double
    */
   public double estimate() {
     double value = calculateTotalUsage(address.getDevicesAsArray()) * rate;

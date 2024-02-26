acc683d7ae7bdea2b9b8245bc81155d04115461f
Danilo Bellini <bellini.daan@gmail.com>
2024-02-26 19:22:58 -0300

    feat: run method implemented in Application + Java docs

--- src/main/java/com/betrybe/minhaconta/presentation/Application.java
+++ src/main/java/com/betrybe/minhaconta/presentation/Application.java
@@ -9,11 +9,19 @@ import com.ions.lightdealer.sdk.service.LightDealerApi;
  */
 public class Application {
 
+  /**
+   * The Ui.
+   */
   ConsoleUserInterface ui;
+  /**
+   * The Api.
+   */
   LightDealerApi api;
 
   /**
    * Constructor that instantiates a new Application.
+   *
+   * @param ui the ui
    */
   public Application(ConsoleUserInterface ui) {
     this.ui = ui;
@@ -24,10 +32,27 @@ public class Application {
    * Req. 4 – Creates CLI menu.
    */
   public void run() {
+    char optionSelected = '0';
+    while (optionSelected != '6') {
+      String[] optionsMenu;
+      optionsMenu = new String[]{
+          "1 - Cadastrar cliente",
+          "2 - Cadastrar imóvel de cliente",
+          "3 - Cadastrar dispositivos em imóvel",
+          "4 - Estimar conta de imóvel",
+          "5 - Otimizar uso de energia",
+          "6 - Sair"
+      };
+
+      optionSelected = ui.inputMenuOption(optionsMenu);
+      runOptionAction(optionSelected);
+    }
   }
 
   /**
    * Req. 5 – Run menu options.
+   *
+   * @param option the option
    */
   public void runOptionAction(char option) {
   }
@@ -64,6 +89,8 @@ public class Application {
 
   /**
    * Req 10 - Aux. Method to display high consumptions devices.
+   *
+   * @param energyAccount the energy account
    */
   public void suggestReducedUsage(EnergyAccount energyAccount) {
   }
